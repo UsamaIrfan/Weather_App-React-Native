84afb441cd97f6b6ef276567bac47db6fafba2d9
Usama Irfan <usama.irfan9797@gmail.com>
2020-11-26 01:05:22 +0500

    Initial

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View , Button} from 'react-native';
 
 export default function App() {
+
+  const [test, settest] = useState("This is working")
+  const [count, setcount] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.lightText}>{test}</Text>
+      <Text style={styles.largeText}>This is so cool!</Text>
+      <Text style={styles.largeText}>{count}</Text>
+      <Button
+        onPress={() => setcount(count + 1)}
+        title="Add"
+        style={styles.button}
+      />
+      <Button
+        onPress={() => setcount(count - 1)}
+        title="Remove"
+        style={styles.button}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +33,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  lightText: {
+    color: "#000"
+  },
+  largeText: {
+    fontSize: 20,
+    color: "#000",
+  },
+  button: {
+    padding: 10 + "px"
+  }
 });
